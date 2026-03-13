18d4c65f88045e18b5c36603ae1e3b53f892440d
duyhuy25 <duyhuy1342005@gmail.com>
2026-03-13 19:53:13 +0700

    update container

--- btl_qlkhobai_FontEnd/src/pages/Containers.tsx
+++ btl_qlkhobai_FontEnd/src/pages/Containers.tsx
@@ -1,4 +1,28 @@
+import React, { useEffect, useState } from "react";
+import "./Containers.css";
+
+interface Container {
+  ContainerID: number;
+  LoaiHang: string;
+  TrongLuong: number;
+  TrangThai: string;
+  Kho: string;
+  PhuongTien: string;
+  HopDong: string;
+}
+
 const Containers = () => {
+
+  const [containers, setContainers] = useState<Container[]>([]);
+
+  useEffect(() => {
+
+    fetch("http://localhost:5000/api/containers")
+      .then(res => res.json())
+      .then(data => setContainers(data))
+      .catch(err => console.error(err));
+
+  }, []);
 
   return (
 
@@ -10,27 +34,39 @@ const Containers = () => {
 
         <thead>
           <tr>
-          <th>ID</th>
-          <th>Loại hàng</th>
-          <th>Trọng lượng (kg)</th>
-          <th>Trạng thái</th>
-          <th>Kho</th>
-          <th>Phương tiện</th>
-          <th>Hợp đồng</th>
-          <th>Hành động</th>
+            <th>ID</th>
+            <th>Loại hàng</th>
+            <th>Trọng lượng</th>
+            <th>Trạng thái</th>
+            <th>Kho</th>
+            <th>Phương tiện</th>
+            <th>Hợp đồng</th>
+            <th>Hành động</th>
           </tr>
         </thead>
 
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Điện tử</td>
-            <td>500kg</td>
-            <td>Trong kho</td>
-            <td>Kho Bắc Ninh</td>
-            <td>89H7789</td>
-            <td>Tiền Tỷ</td>
-          </tr>
+
+          {containers.map((c) => (
+
+            <tr key={c.ContainerID}>
+              <td>{c.ContainerID}</td>
+              <td>{c.LoaiHang}</td>
+              <td>{c.TrongLuong} kg</td>
+              <td>{c.TrangThai}</td>
+              <td>{c.Kho}</td>
+              <td>{c.PhuongTien}</td>
+              <td>{c.HopDong}</td>
+
+              <td>
+                <button className="btn-edit">Sửa</button>
+                <button className="btn-delete">Xóa</button>
+              </td>
+
+            </tr>
+
+          ))}
+
         </tbody>
 
       </table>
